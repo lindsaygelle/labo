89c400dbe2979b07f4d605b9fb31184d655b1bb7
gellel <lindsay.gelle@gmail.com>
2019-09-15 19:05:46 +1000

    Amending LaboKit struct

--- laboKit.go
+++ laboKit.go
@@ -1,14 +1,4 @@
 package labo
 
 type LaboKit struct {
-	Available     bool
-	Compatability []interface{}
-	Description   string
-	KitFeatures   []interface{}
-	PictureURL    string
-	Price         string
-	Projects      []interface{}
-	QuoteList     []interface{}
-	SourceSet     []string
-	URL           string
 }
