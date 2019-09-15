cd02d3b9d7b19bbc46689d36901245c94a22045f
gellel <lindsay.gelle@gmail.com>
2019-09-15 14:47:44 +1000

    First commit

---
+++ laboKit.go
@@ -0,0 +1,14 @@
+package labo
+
+type LaboKit struct {
+	Available     bool
+	Compatability []interface{}
+	Description   string
+	KitFeatures   []interface{}
+	PictureURL    string
+	Price         string
+	Projects      []interface{}
+	QuoteList     []interface{}
+	SourceSet     []string
+	URL           string
+}
---
+++ laboVRKit.go
@@ -0,0 +1,4 @@
+package labo
+
+type LaboVRKit struct {
+}
