54845926b1a9a1958fb66c2885d313ec3ac645a5
gellel <lindsay.gelle@gmail.com>
2019-09-16 21:31:27 +1000

    Adding properties to LaboKitBuyAt struct

--- laboKitBuyAt.go
+++ laboKitBuyAt.go
@@ -1,23 +1,64 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type LaboKitBuyAt struct {
-	Name   string
-	Target string
+	Company         string
+	CompanyImageURL string
+	Href            string
+	Target          string
+	URL             *url.URL
 }
 
-func NewLaboKitBuyAt(s *goquery.Selection) (*LaboKitBuyAt, error) {
-	if ok := len(s.Nodes) != 0; ok != true {
-		return nil, errors.New("HTML childNodes cannot be empty")
+func NewLaboKitBuyAt(s *goquery.Selection) *LaboKitBuyAt {
+	return &LaboKitBuyAt{
+		Company:         parseLaboKitBuyAtCompany(s),
+		CompanyImageURL: parseLaboKitBuyAtCompayImageURL(s),
+		Href:            parseLaboKitBuyAtHref(s),
+		Target:          parseLaboKitBuyAtTarget(s),
+		URL:             parseLaboKitBuyAtURL(s)}
+}
+
+func parseLaboKitBuyAtCompany(s *goquery.Selection) string {
+	attribute := s.Find("img").AttrOr("alt", "NIL")
+	substring := strings.TrimSpace(attribute)
+	substring = strings.ReplaceAll(substring, " ", "-")
+	return strings.ToUpper(substring)
+}
+
+func parseLaboKitBuyAtCompayImageURL(s *goquery.Selection) string {
+	attribute := s.Find("img").AttrOr("src", "NIL")
+	if ok := attribute != "NIL"; ok != true {
+		return attribute
 	}
-	if ok := strings.ToUpper(s.Nodes[0].Data) == "A"; ok != true {
-		return nil, errors.New("HTML must be an anchor tag")
+	substring := strings.TrimSpace(attribute)
+	substring = strings.ReplaceAll(substring, "../", "")
+	return fmt.Sprintf("https://labo.nintendo.com/%s", substring)
+}
+
+func parseLaboKitBuyAtHref(s *goquery.Selection) string {
+	attribute := s.AttrOr("href", "NIL")
+	substring := strings.TrimSpace(attribute)
+	return strings.ToUpper(substring)
+}
+
+func parseLaboKitBuyAtTarget(s *goquery.Selection) string {
+	attribute := s.AttrOr("target", "NIL")
+	substring := strings.TrimSpace(attribute)
+	return strings.ToUpper(substring)
+}
+
+func parseLaboKitBuyAtURL(s *goquery.Selection) *url.URL {
+	attribute := s.AttrOr("href", "NIL")
+	URL, err := url.Parse(attribute)
+	if err != nil {
+		return new(url.URL)
 	}
-	return nil, nil
+	return URL
 }
