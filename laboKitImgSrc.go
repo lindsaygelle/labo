d11c44b9cc292c0bb8c0da0e6f7197435edb94fc
gellel <lindsay.gelle@gmail.com>
2019-09-17 23:07:18 +1000

    Adding image srcset measurement to LaboKitImgSrc struct

--- laboKitImgSrc.go
+++ laboKitImgSrc.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 type LaboKitImgSrc struct {
-	At   int      `json:"at"`
-	Href string   `json:"href"`
-	URL  *url.URL `json:"URL"`
+	At          int      `json:"at"`
+	Href        string   `json:"href"`
+	Measurement string   `json:"measurement"`
+	URL         *url.URL `json:"URL"`
 }
 
 func NewLaboKitImgSrc(s string) *LaboKitImgSrc {
@@ -21,17 +23,21 @@ func NewLaboKitImgSrc(s string) *LaboKitImgSrc {
 	s1 := strings.TrimSpace(substrings[0])
 	href := strings.ReplaceAll(s1, "../", "")
 	href = fmt.Sprintf("%s/%s", nintendoLaboURL, href)
+
+	fmt.Println(href)
 	URL, err := url.Parse(href)
 	if err != nil {
 		return nil
 	}
 	s2 := strings.TrimSpace(substrings[1])
-	at, err := strconv.Atoi(strings.TrimSuffix(s2, "w"))
+	at, err := strconv.Atoi(string(s2[:1]))
 	if err != nil {
 		return nil
 	}
+	measurement := regexp.MustCompile(`\d+`).ReplaceAllString(s2, "")
 	return &LaboKitImgSrc{
-		At:   at,
-		Href: href,
-		URL:  URL}
+		At:          at,
+		Href:        href,
+		Measurement: measurement,
+		URL:         URL}
 }
