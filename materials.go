8e3ac47da16360c67b46b0761e875da6afd280d5
gellel <lindsay.gelle@gmail.com>
2019-09-21 15:36:41 +1000

    Updating Materials documentation

--- materials.go
+++ materials.go
@@ -6,20 +6,34 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+const (
+	materialsPartsCSSSelector string = ".contents-content ul li"
+	materialsRootCSSSelector  string = ".contents .right-column"
+	materialsImageCSSSelector string = ".kit-contents picture img"
+)
+
+// Materials is a representation of the parts required to build a Nintendo Labo ToyCon.
 type Materials struct {
 	Image *Image
 	Parts []*Part
 }
 
+// NewMaterials is a constructor function that instantiates a new Materials pointer.
 func NewMaterials(s *goquery.Selection) (*Materials, error) {
+	var (
+		image *Image
+		parts []*Part
+	)
+	if ok := (s != nil); !ok {
+		return nil, fmt.Errorf(errorGoQuerySelectionNil)
+	}
 	if ok := (s.Length() > 0); !ok {
-		return nil, fmt.Errorf("goquery.Selection is empty")
+		return nil, fmt.Errorf(errorGoQuerySlectionEmptyHTMLNodes, s)
 	}
-	partsSelection := s.Find(".contents-content ul li")
+	partsSelection := s.Find(materialsPartsCSSSelector)
 	if ok := (partsSelection.Length() > 0); !ok {
-		return nil, fmt.Errorf("goquery.Selection is empty")
+		return nil, fmt.Errorf(errorGoQuerySlectionEmptyHTMLNodes, partsSelection)
 	}
-	parts := []*Part{}
 	partsSelection.Each(func(i int, s *goquery.Selection) {
 		part, err := NewPart(s)
 		if err != nil {
@@ -27,9 +41,9 @@ func NewMaterials(s *goquery.Selection) (*Materials, error) {
 		}
 		parts = append(parts, part)
 	})
-	imageSelection := s.Find(".kit-contents picture img")
+	imageSelection := s.Find(materialsImageCSSSelector)
 	if ok := (imageSelection.Length() > 0); !ok {
-		return nil, fmt.Errorf("goquery.Selection is empty")
+		return nil, fmt.Errorf(errorGoQuerySlectionEmptyHTMLNodes, imageSelection)
 	}
 	image, err := NewImage(imageSelection)
 	if err != nil {
