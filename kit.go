355a52fd492726d442206b7d8aa93761aadce304
gellel <lindsay.gelle@gmail.com>
2019-09-22 20:54:02 +1000

    Adding features

--- kit.go
+++ kit.go
@@ -12,6 +12,7 @@ const (
 	kitNameCSSSelector      string = "h1.visually-hidden"
 	kitRetailersCSSSelector string = "div.retailers ul li"
 	kitPriceCSSSelector     string = "p.price"
+	kitToyConCSSSelector    string = "div.toy-con-area .toy-con"
 )
 
 // Kit is a struct that is the entirety of a Nintendo Labo Toy Con kit.
@@ -45,9 +46,8 @@ func NewKit(s *goquery.Selection) (*Kit, error) {
 		materials, _ = NewMaterials(s.Find(materialsRootCSSSelector))
 		overview, _  = NewOverview(s.Find(overviewRootCSSSelector))
 		software, _  = NewSoftware(s.Find(softwareRootCSSSelector))
-		price, e     = NewPrice(s.Find(kitPriceCSSSelector))
+		price, _     = NewPrice(s.Find(kitPriceCSSSelector))
 	)
-	fmt.Println(e)
 	nameSelection := s.Find(kitNameCSSSelector)
 	if ok := (nameSelection.Length() > 0); ok {
 		name = strings.TrimSpace(nameSelection.Text())
@@ -60,6 +60,10 @@ func NewKit(s *goquery.Selection) (*Kit, error) {
 			return
 		}
 		retailers = append(retailers, retailer)
+	})
+	toyConSelection := s.Find(kitToyConCSSSelector)
+	toyConSelection.Each(func(i int, s *goquery.Selection) {
+		fmt.Println(i)
 	})
 	kit := Kit{
 		BoxImage:  boxImage,
--- toyCon.go
+++ toyCon.go
@@ -1,8 +1,16 @@
 package labo
 
+import (
+	"github.com/PuerkitoBio/goquery"
+)
+
 type ToyCon struct {
 	Description string
 	Features    []*Feature
 	Image       *Image
 	Name        string
+}
+
+func NewToyCon(s *goquery.Selection) {
+
 }
