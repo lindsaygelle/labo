c1b85453737e4f5a79f6d8710e6052e3ab379652
gellel <lindsay.gelle@gmail.com>
2019-09-26 21:36:04 +1000

    Adding CSSSelector consts

--- project.go
+++ project.go
@@ -1,12 +1,19 @@
 package labo
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const ()
+const (
+	projectIconCSSSelector        string = ".toycon-icon img"
+	projectImageCSSSelector       string = ".toycon-image picture img"
+	projectNameCSSSelector        string = ".toycon-icon p"
+	projectScreenshotCSSSelector  string = "picture img"
+	projectScreenshotsCSSSelector string = ".screenshot"
+)
 
 // Project is a unique build project that is bundled with a Nintendo Labo Kit.
 type Project struct {
@@ -18,20 +25,26 @@ type Project struct {
 
 // NewProject is a constructor function that instantiates and returns a new Project struct pointer.
 func NewProject(s *goquery.Selection) (*Project, error) {
+	if ok := (s != nil); !ok {
+		return nil, fmt.Errorf(errorGoQuerySelectionNil)
+	}
+	if ok := (s.Length() > 0); !ok {
+		return nil, fmt.Errorf(errorGoQuerySelectionEmptyHTMLNodes, s)
+	}
 	var (
 		name        string
 		screenshots []*Image
 	)
 	var (
-		image, _ = NewImage(s.Find(".toycon-image picture img"))
-		icon, _  = NewImage(s.Find(".toycon-icon img"))
+		icon, _  = NewImage(s.Find(projectIconCSSSelector))
+		image, _ = NewImage(s.Find(projectImageCSSSelector))
 	)
-	nameSelection := s.Find(".toycon-icon p")
+	nameSelection := s.Find(projectNameCSSSelector)
 	name = strings.TrimSpace(nameSelection.Text())
 	name = strings.ToUpper(name)
-	screenshotsSelection := s.Find(".screenshot")
+	screenshotsSelection := s.Find(projectScreenshotsCSSSelector)
 	screenshotsSelection.Each(func(i int, s *goquery.Selection) {
-		screenshot, err := NewImage(s.Find("picture img"))
+		screenshot, err := NewImage(s.Find(projectScreenshotCSSSelector))
 		if err != nil {
 			return
 		}
