41693a7653b8896bddc902675bd1528dc3ad1a0b
gellel <lindsay.gelle@gmail.com>
2019-09-26 23:01:48 +1000

    Adding customization parts to Labo kit

---
+++ customizationPart.go
@@ -0,0 +1,7 @@
+package labo
+
+type CustomizationPart struct {
+	Amount int
+	Image  *Image
+	Name   string
+}
--- kitCustomization.go
+++ kitCustomization.go
@@ -1,12 +1,62 @@
 package labo
 
 import (
+	"fmt"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
+const (
+	errorKitCustomizationNoBoxImage  string = ""
+	errorKitCustomizationNoMaterials string = ""
+	errorKitCustomizationNoPrice     string = ""
+)
+
+const (
+	kitCustomizationBoxImageCSSSelector  string = "div.packshot picture img"
+	kitCustomizationMaterialsCSSSelector string = "section.contents .row"
+	kitCustomizationPriceCSSSelector     string = "div.price .price-card .price"
+)
+
 type KitCustomization struct {
+	BoxImage  *Image
+	Href      string
+	Materials []*KitCustomization
+	Price     *Price
 }
 
 func NewKitCustomization(s *goquery.Selection) (*KitCustomization, error) {
-	return nil, nil
+	if ok := (s != nil); !ok {
+		return nil, fmt.Errorf(errorGoQuerySelectionNil)
+	}
+	if ok := (s.Length() > 0); !ok {
+		return nil, fmt.Errorf(errorGoQuerySelectionEmptyHTMLNodes, s)
+	}
+	var (
+		boxImage  *Image
+		href      string
+		materials []*KitCustomization
+		price     *Price
+	)
+	imageSelection := s.Find(kitCustomizationBoxImageCSSSelector)
+	if ok := (imageSelection.Length() > 0); !ok {
+		return nil, fmt.Errorf(errorKitCustomizationNoBoxImage)
+	}
+	materialsSelection := s.Find(kitCustomizationMaterialsCSSSelector)
+	if ok := (materialsSelection.Length() > 0); !ok {
+		return nil, fmt.Errorf(errorKitCustomizationNoMaterials)
+	}
+	materialsSelection.Each(func(i int, s *goquery.Selection) {
+
+	})
+	priceSelection := s.Find(kitCustomizationPriceCSSSelector)
+	if ok := (priceSelection.Length() > 0); !ok {
+		return nil, fmt.Errorf(errorKitCustomizationNoPrice)
+	}
+	kitCustomization := KitCustomization{
+		BoxImage:  boxImage,
+		Href:      href,
+		Materials: materials,
+		Price:     price}
+	return &kitCustomization, nil
 }
